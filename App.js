89e645371a844d71ceab274829ae4cd24baf1ec7
Adam Sarli <adam.sarli@gmail.com>
2020-03-18 11:24:06 -0400

    added store, redux, reducers and actions from seating-chart project

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import KlassesScreen from './src/screens/KlassesScreen'
+import { store } from './src/store.js'
 
 const navigator = createStackNavigator({
   Klasses: KlassesScreen
